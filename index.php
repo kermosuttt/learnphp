5382a0793a2b8a30b2fc685e7673d49cb382b689
Kermo Sutt <kermo.sutt@tptlive.ee>
2024-09-05 10:20:21 +0300

    alphabet

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+$a = 1;
+$a++;
+$a--;
+$a+=2;
+$a-=2;
+$a*=2;
+$a/=2;
+$a%=2;
+
+$a = 'hello ' . 'world';
+$a .='asdaskdj';
+$array = array(1,2,3);
+$array = [1,2,3];
+
+var_dump($array[1]);
+$array = [
+    'essa' => 1,
+   'tessa' => 2,
+   'kossa' => 3,
+    5 => 4,
+    'asdasd',
+    10 => 1.2,
+    true
+];
+var_dump($array['kossa']);
+
+var_dump($array);
+// foreach($array as $value){
+//     echo $value . "\n";
+// }
+foreach($array as $key => $value){
+    //echo $key . '=>'. $value . "\n";
+    echo "$key=>$value\n";
+}
+$name = 'Kermo';
+
+$string = "hello $name\n";
+var_dump($string);
+
+$char = 'a';
+
+for($i=0;$i<100;$i++){
+    var_dump($char);
+    $char++;
+}
