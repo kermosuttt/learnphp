774463569d233bd7e3c2a4bf4e3113eaead601d8
kermosutt <kermo.sutt@tptlive.ee>
2024-09-06 12:52:17 +0300

    objects

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
 
-$array = [1,2,3,4];
-array_push($array,5,6,7);
-$array[] = 8;
-$array[4] = 123123;
-var_dump($array);
+class Box {
+    public $height;
+    public $width;
+    public $length;
 
-function hello($name=''){
-    echo "hello $name\n";    
+    public function volume(){
+        return $this->height * $this->width * $this->length;
+    } 
 }
 
-hello('kermo');
-hello();
+$box1 = new Box();
+$box1->height = 1;
+$box1->width = 2;
+$box1->length = 3;
+var_dump($box1);
+$box2 = new Box();
+$box2->height = 4;
+$box2->width = 5;
+$box2->length = 6;
+var_dump($box1);
+var_dump($box2);
+
+var_dump($box1->volume());
+var_dump($box2->volume());
