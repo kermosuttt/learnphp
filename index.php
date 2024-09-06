ed1da2222b5863802a4dc9d76662aa55025d30f8
kermosutt <kermo.sutt@tptlive.ee>
2024-09-06 12:55:43 +0300

    array push

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
 
-class Box {
-    public $height;
-    public $width;
-    public $length;
-
-    public function volume(){
-        return $this->height * $this->width * $this->length;
-    } 
-}
-
-class MetalBox extends Box {
-    public $weightPerUnit = 10;
-    public function weight(){
-        return $this->volume() * $this->weightPerUnit;
-    }
-}
-
-$num1 = 1;
-$num2 = $num1;
-$num2 = 2;
-var_dump($num1, $num2);
-
-
-
-$metal1 = new MetalBox();
-$metal1->height = 1;
-$metal2 = clone $metal1;
-$metal2->height = 2;
-var_dump($metal1, $metal2);
+$array = [1,2,3,4];
+array_push($array,5,6,7);
+$array[] = 8;
+$array[4] = 123123;
+var_dump($array);
