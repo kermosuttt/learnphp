77621a5878eb8aeb6834bb00e2ac576e0cdcd239
kermosutt <kermo.sutt@tptlive.ee>
2024-09-06 14:50:17 +0300

    static

--- index.php
+++ index.php
@@ -1,37 +1,20 @@
 <?php
 
 class Box {
-    public $men = 'cool';
+    public static $count = 0;
+    public $id;
     public function __construct(){
-        echo "Box was created\n";
+        $this->id = self::$count++;
     }
-
-    public function __call($name, $args){
-        var_dump($name, $args);
-    } 
-    public function __get($name){
-        var_dump($name);
-        return 'cool value';
+    public static function setCount(){
+        //var_dump($this->id);
+        var_dump(self::$count);
     }
-    public function __set($name, $value){
-        var_dump($name, $value);
-    }
-    public function __destruct(){
-        echo "Box was destoryed\n";
-    }
-    public function __toString(){
-        return 'cool box';
-    }
-    public function __invoke(...$args){
-        var_dump($args);
+    public static function name(){
+        var_dump(static::class);
     }
 }
-
-$box = new Box();
-$box->fname = 'kermo';
-$box->something(1,2,'asd');
-$box->else('xxd', true, NULL);
-var_dump($box->alahsdha);
-var_dump($box);
-echo $box;
-$box(1,'asd', 'asda');
+class MetalBox extends Box {
+    
+}
+MetalBox::name();
