6e171b91f8828c0ce4f8919586a0a564e517f144
kermosutt <kermo.sutt@tptlive.ee>
2024-09-06 12:54:34 +0300

    objects assignment vs simple values

--- index.php
+++ index.php
@@ -10,17 +10,22 @@ class Box {
     } 
 }
 
-$box1 = new Box();
-$box1->height = 1;
-$box1->width = 2;
-$box1->length = 3;
-var_dump($box1);
-$box2 = new Box();
-$box2->height = 4;
-$box2->width = 5;
-$box2->length = 6;
-var_dump($box1);
-var_dump($box2);
+class MetalBox extends Box {
+    public $weightPerUnit = 10;
+    public function weight(){
+        return $this->volume() * $this->weightPerUnit;
+    }
+}
 
-var_dump($box1->volume());
-var_dump($box2->volume());
+$num1 = 1;
+$num2 = $num1;
+$num2 = 2;
+var_dump($num1, $num2);
+
+
+
+$metal1 = new MetalBox();
+$metal1->height = 1;
+$metal2 = clone $metal1;
+$metal2->height = 2;
+var_dump($metal1, $metal2);
